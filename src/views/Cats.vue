8531d20a33b34fcc49049b0a1130fb647d5028c7
DavideDR <d.dr@libero.it>
2020-08-20 10:47:24 +0200

    Adds bootstrap imports and data

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,3 +1,18 @@
 <template>
     <div>Cats</div>
+    <div>
+        <b-table striped hover :items="items"></b-table>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                
+            ]
+        }
+    }
+}
+</script>
