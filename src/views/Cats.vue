89314f9b37bd27511a403a73a8625862a14ec642
DavideDR <d.dr@libero.it>
2020-08-20 11:58:36 +0200

    Adds links for exploring a pet from Cats and Dogs view

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -2,7 +2,11 @@
     <div>
         <div><h1>Cats</h1></div>
         <div>
-            <b-table striped hover :items="cats"></b-table>
+            <b-table striped hover :items="cats">
+                <template v-slot:cell(name)="data">
+                    <router-link v-bind:to="`/pets/${data.index}`">{{ data.value }}</router-link>
+                </template>
+            </b-table>
         </div>
     </div>
 </template>
@@ -11,7 +15,7 @@
 // @ is a Vue-cli shortcut reference to the src folder, to avoid writing ../data/
 import cats from '@/data/cats'
 export default {
-  data() {
+  data () {
     return {
       // short hand for cats:cats
       cats
