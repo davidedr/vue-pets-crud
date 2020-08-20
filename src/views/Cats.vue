e6c614a75828938032e0798699f1b4d6efc48df9
DavideDR <d.dr@libero.it>
2020-08-20 11:21:25 +0200

    Adds Cats, Dogs data and table

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,18 +1,21 @@
 <template>
-    <div>Cats</div>
     <div>
-        <b-table striped hover :items="items"></b-table>
+        <div><h1>Cats</h1></div>
+        <div>
+            <b-table striped hover :items="cats"></b-table>
+        </div>
     </div>
 </template>
 
 <script>
+// @ is a Vue-cli shortcut reference to the src folder, to avoid writing ../data/
+import cats from '@/data/cats'
 export default {
-    data() {
-        return {
-            items: [
-                
-            ]
-        }
+  data() {
+    return {
+      // short hand for cats:cats
+      cats
     }
+  }
 }
 </script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Dogs</div>
+    <div>
+        <div><h1>Dogs</h1></div>
+        <div>
+            <b-table striped hover :items="dogs"></b-table>
+        </div>
+    </div>
 </template>
+
+<script>
+// @ is a Vue-cli shortcut reference to the src folder, to avoid writing ../data/
+import dogs from '@/data/dogs'
+export default {
+  data() {
+    return {
+      // short hand for cats:cats
+      dogs
+    }
+  }
+}
+</script>
